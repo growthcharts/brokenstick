a0453a710eed7e2c16b618737ddfa6b42e7a6fd7
Stef van Buuren <stef.vanbuuren@tno.nl>
2015-08-17 22:48:38 +0200

    preterm refs added

--- R/scales.R
+++ R/scales.R
@@ -5,7 +5,9 @@
 #' @aliases convert2y
 #' @param yname a string (e.g. \code{"hgt"} or \code{"wgt"}) indicating the outcome variable
 #' @param z The broken stick values in the Z-score scale
-#' @param sex either \code{"male"} or \code{"female"}
+#' @param sex factor of \code{length(z)} elements, either \code{"male"} or \code{"female"}
+#' @param ga numerical vector giving the gestional age in weeks used to calculate Z-score for preterms (e.g. when \code{preterm = TRUE})
+#' @param preterm logical indicating whether the preterm references should be used
 #' @param breaks a vector of internal knots
 #' @param Boundary.knots vector of external knots
 #' @return A \code{matrix} of the same dimensions as \code{bs}
@@ -13,6 +15,8 @@
 convert2y <- function(yname = "hgt", 
 					  z,
 					  sex, 
+					  ga = NULL,
+					  preterm = FALSE,
 					  breaks = round(c(0, 28/365.25, 56/365.25, 
 					  				 1/4, 1/3, 1/2, 7.5/12,
 					  				 9/12, 11/12, 14/12, 18/12, 2), 4),
@@ -22,6 +26,10 @@ convert2y <- function(yname = "hgt",
 	sex <- rep(sex, length(breaks) + 1)
 	y <- z2y(z = z, x = x, ref = "nl1997", yname = yname,
 			 sex = sex, sub = 'NL', drop = TRUE)
+	if (preterm)
+		y <- z2y(z = z, x = x, ref = "preterm", yname = yname,
+					sex = sex, sub = max(floor(ga), 25), drop = TRUE)
+	
 	y <- matrix(y, nrow = length(z))
 	colnames(y) <- paste(yname, 0:length(breaks), sep = "")
 	y
