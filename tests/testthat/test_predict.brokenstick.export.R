d6daae75bce70c5e1838f592243005572cc41394
Stef van Buuren <stef.vanbuuren@tno.nl>
2016-08-04 09:03:13 -0500

    Add test for unordered x

--- tests/testthat/test_predict.brokenstick.export.R
+++ tests/testthat/test_predict.brokenstick.export.R
@@ -1,9 +1,9 @@
 
 library(brokenstick)
-context("Arguments of predict.brokenstick.export()")
+context("predict()")
 
 exp <- export.brokenstick(fit.hgt)
-test_that("predict() returns proper number of rows", {
+test_that("returns proper number of rows", {
   expect_equal(nrow(predict(exp, x = NA)), 1)
   expect_equal(nrow(predict(exp, x = NA, y = 10)), 1)
   expect_equal(nrow(predict(exp, x = c(NA, NA), y = c(-1, 10))), 2)
@@ -11,14 +11,26 @@ test_that("predict() returns proper number of rows", {
 })
 
 meanmodel <- predict(exp)
-test_that("predict() returns mean model", {
+test_that("returns mean model", {
   expect_equal(predict(exp, x = NA, type = "atknots"), meanmodel)
+  expect_equal(predict(exp, x = c(NA, NA), type = "atknots"), meanmodel)
 })
 
+ test_that("accepts intermediate NA in x", {
+   expect_equal(
+     unlist(predict(exp, x = 1, y = -1)[1,]), 
+     unlist(predict(exp, x = c(NA, 1), y = c(1, -1))[2,]))
+   expect_equal(
+     unlist(predict(exp, x = c(1, NA), y = c(-1, 1))[1,]), 
+     unlist(predict(exp, x = c(NA, 1), y = c(1, -1))[2,]))
+   expect_equal(
+     unlist(predict(exp, x = c(1, 2, NA), y = c(NA, -1, 1))[2, ]),
+     unlist(predict(exp, x = c(1, NA, 2), y = c(NA,  1,-1))[3, ]))
+})
 
-# desired <- predict(exp, x = c(1, NA), y = c(-1, 1))
-# test_that("predict.brokenstick.export() steps over missing x", {
-#   expect_equal(predict(exp, x = c(1, NA), y = c(-1, 1)), desired)
-#   expect_equal(predict(exp, x = c(NA, 1), y = c(-1, 1)), desired)
-#   expect_equal(predict(exp, x = c(NA, 1, 2), y = c(NA, NA, NA)), desired)
-# })
+ test_that("accepts unordered x", {
+   expect_equal(
+     unlist(predict(exp, x = c(1, 2, 3), y = c(-1, 1, 0))[3,]), 
+     unlist(predict(exp, x = c(2, 3, 1), y = c(1, 0, -1))[2,]))
+ })
+ 
