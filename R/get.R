62dc66349f9e229d06bbb56f7ec8213c4e80e688
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-08 22:15:35 +0200

    Style file

--- R/get.R
+++ R/get.R
@@ -1,16 +1,19 @@
 #' Obtain the knots from a broken stick model
 #'
-#' @param object An object of class \code{brokenstick} or \code{brokenstick.export}
+#' @param object An object of class \code{brokenstick} or
+#' \code{brokenstick.export}
 #' @param what A character vector of length 1. Valid values are
-#' \code{"all"}, \code{"knots"}, \code{"boundary"} or \code{"droplast"}. The default is
-#' \code{what = "all"}.
-#' @return A vector with knot locations, either both, internal only or boundary only.
-#' The result is \code{NULL} if \code{object} does not have proper class. The function
-#' can return \code{numeric(0)} if there are no internal knots.
+#' \code{"all"}, \code{"knots"}, \code{"boundary"} or \code{"droplast"}.
+#' The default is \code{what = "all"}.
+#' @return A vector with knot locations, either both, internal only or
+#' boundary only. The result is \code{NULL} if \code{object} does not
+#' have proper class. The function can return \code{numeric(0)} if
+#' there are no internal knots.
 #' @examples
 #' get_knots(fit_206, "knots")
 #' @export
-get_knots <- function(object, what = c("all", "knots", "boundary", "droplast")) {
+get_knots <- function(object,
+                      what = c("all", "knots", "boundary", "droplast")) {
 
   if (!inherits(object, c("brokenstick", "brokenstick_export"))) return(NULL)
 
@@ -37,8 +40,10 @@ get_knots <- function(object, what = c("all", "knots", "boundary", "droplast")) 
 #' Obtain the x and y data from a broken stick model
 #'
 #' @param object An object of class \code{brokenstick}
-#' @param ids A vector specifying the id's of the persons. If omitted, all id's are included.
-#' @return A data frame with subjid, x and y. The result is \code{NULL} if \code{object} is not of class \code{brokenstick}.
+#' @param ids A vector specifying the id's of the persons. If omitted,
+#' all id's are included.
+#' @return A data frame with subjid, x and y. The result is \code{NULL}
+#' if \code{object} is not of class \code{brokenstick}.
 #' @examples
 #' get_xy(fit_206, ids = c(10001, 10002))
 #' @export
@@ -51,9 +56,11 @@ get_xy <- function(object, ids = NULL) {
 #' Obtain the X model matrix from a broken stick model
 #'
 #' @param object An object of class \code{brokenstick}
-#' @param ids A vector specifying the id's of the persons. If omitted, all id's are included.
-#' @return A matrix with number of columns equal to the number of knots.
-#' The result is \code{NULL} if \code{object} is not of class \code{brokenstick}.
+#' @param ids A vector specifying the id's of the persons. If omitted,
+#' all id's are included.
+#' @return A matrix with number of columns equal to the number of
+#' knots. The result is \code{NULL} if \code{object} is not of class
+#' \code{brokenstick}.
 #' @examples
 #' get_X(fit_206, ids = c(10001, 10002))
 #' @export
