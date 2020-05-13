8fa0ac576565d200408ec2ef70883829557fe172
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-13 16:10:18 +0200

    Simplify package by supporting ggplot2 only

--- R/plot.R
+++ R/plot.R
@@ -16,11 +16,9 @@
 #' @param estimates A logical indicating whether the broken stick estimates should be plotted.
 #' By default, it is \code{TRUE} if there are internal knots, and \code{FALSE} if there are
 #' only boundary knots.
-#' @param ... Extra arguments passed down to \code{\link[rbokeh]{figure}} and
-#' \code{\link[rbokeh]{ly_lines}}, \code{\link[rbokeh]{ly_points}},
-#' '\code{\link[hbgd]{ly_zband}} and '\code{\link[rbokeh]{grid_plot}} functions.
-#' @return An object of class \code{rbokeh}.
-#' @author Stef van Buuren 2017
+#' @param ... Extra arguments.
+#' @return An object of class \code{ggplot}.
+#' @author Stef van Buuren 2020
 #' @method plot brokenstick
 #' @examples
 #' library("brokenstick")
@@ -30,21 +28,17 @@
 #'
 #' # plot measurements for first three cases
 #' plot(fit, zband = FALSE, est = FALSE)
-#' plot(fit, zband = FALSE, est = FALSE, pkg = "bokeh", width = 250, height = 350)
 #'
 #' # fit model with knots at 1 and 2 years
-#' fit <- brokenstick(y = dat$hgt.z, x = dat$age, subj = dat$subjid, knots = 1:2)
+#' fit <- brokenstick(y = dat$hgt.z, x = dat$age, subj = dat$subjid,
+#'   knots = 1:2)
 #'
-#' # ggplot
 #' plot(fit, xlim = c(0, 2.2), ylim = c(-3, 3))
 #' plot(fit, ids = c(10005, 10012), xlim = c(0, 2.2))
 #'
-#' # bokeh plots with using x_trim
-#' plot(fit, pkg = "bokeh", width = 250, height = 350, x_trim = c(0, 2.2))
-#'
 #' # bokeh plots with xlim and ylim
-#' plot(fit, xlim = c(0, 2), ylim = c(-3, 3), pkg = "b", width = 250, height = 350)
-#' plot(fit, ids = 10005, pkg = "bokeh", width = 350, height = 300)
+#' plot(fit, xlim = c(0, 2), ylim = c(-3, 3))
+#' plot(fit, ids = 10005)
 #' @export
 plot.brokenstick <- function(x, py, px, ids = NULL,
                              max_ids = 3,
@@ -120,17 +114,12 @@ plot.brokenstick_export <- function(x, py, px, ids = NULL,
 #' @param ylab The label of the y-axis
 #' @param xlim Vector of length 2 with range of x-axis
 #' @param ylim Vector of length 2 with range of y-axis
-#' @param theme Plotting theme (only ggplot)
+#' @param theme Plotting theme
 #' @param zband A logical indicating whether the Z-score band should be
 #' added to the plot. The default is \code{TRUE}.
 #' @param zband_range a vector specifying the range (min, max) that the superposed growth standard should span on the x-axis. The
 #' default is the entire data range.
-#' @param pkg A string indicating whether the \code{"ggplot2"} or
-#' \code{"bokeh"} plotting package should be used. The default is \code{"ggplot2"}.
-#' @param \dots Parameters passed down to \code{\link[rbokeh]{figure}},
-#' \code{\link[rbokeh]{ly_lines}}, \code{\link[rbokeh]{ly_points}}
-#' and '\code{\link[rbokeh]{grid_plot}} functions.
-#' @return An object of class \code{ggplot} or \code{rbokeh}.
+#' @return An object of class \code{ggplot}
 #' @rdname plot_trajectory
 #' @examples
 #' smc <- brokenstick::smocc_50
@@ -140,97 +129,19 @@ plot.brokenstick_export <- function(x, py, px, ids = NULL,
 #' # plot first three cases
 #' plot(fit)
 #' @export
-plot_trajectory <- function(x, data, pkg = c("ggplot2", "bokeh"), ...) {
-  pkg <- match.arg(pkg)
-  if (pkg == "ggplot2") {
-    return(plot_trajectory_ggplot(x = x, data = data, ...))
-  }
-  return(plot_trajectory_bokeh(x = x, data = data, ...))
-}
-
-#' @rdname plot_trajectory
-plot_trajectory_bokeh <- function(x, data,
-                                  color.y = c("blue", "grey"),
-                                  size.y = 6,
-                                  color.yhat = c("red", "grey"),
-                                  size.yhat = 6,
-                                  ncol = 3,
-                                  xlab = "Age (years)",
-                                  ylab = "Length (SDS)",
-                                  zband = TRUE,
-                                  zband_range = NULL,
-                                  xlim = NULL,
-                                  ylim = NULL,
-                                  ...) {
-  if (is.null(zband_range)) {
-    if (!is.null(xlim)) {
-      zband_range <- xlim
-    } else {
-      zband_range <- range(data$x, na.rm = TRUE)
-    }
-  }
-
-  plot_one_fig <- function(x) {
-    k <- x$knot
-    fig <- figure(xlab = xlab, ylab = ylab, ...)
-    if (zband) {
-      fig <- hbgd::ly_zband(fig, x = zband_range, z = -c(2.5, 2, 1, 0))
-    }
-    if (any(!k)) {
-      fig <- fig %>%
-        ly_lines(
-          x = x$x[!k], y = x$y[!k],
-          color = color.y[2]
-        ) %>%
-        ly_points(
-          x = x$x[!k], x$y[!k],
-          color = color.y[1], size = size.y
-        )
-    }
-
-    if (any(k)) {
-      fig <- fig %>%
-        ly_lines(
-          x = x$x[k], y = x$yhat[k],
-          color = color.yhat[2]
-        ) %>%
-        ly_points(
-          x = x$x[k], x$yhat[k],
-          color = color.yhat[1], size = size.yhat
-        )
-    }
-    return(fig)
-  }
-
-  # split since rbokeh does not support faceting
-  data <- split(data, as.factor(as.character(data$subjid)), drop = TRUE)
-  figs <- lapply(data, FUN = plot_one_fig)
-
-  # put together different plots
-  if (length(figs) == 1) {
-    return(figs[[1]])
-  }
-  grid_plot(figs,
-    xlim = xlim, ylim = ylim,
-    same_axes = TRUE, simplify_axes = TRUE, ncol = ncol
-  )
-}
-
-#' @rdname plot_trajectory
-plot_trajectory_ggplot <- function(x, data,
-                                   color.y = c("blue", "grey"),
-                                   size.y = 2,
-                                   color.yhat = c("red", "grey"),
-                                   size.yhat = 2,
-                                   ncol = 3,
-                                   xlab = "Age (years)",
-                                   ylab = "Length (SDS)",
-                                   zband = TRUE,
-                                   zband_range = NULL,
-                                   xlim = NULL,
-                                   ylim = NULL,
-                                   theme = ggplot2::theme_light(),
-                                   ...) {
+plot_trajectory  <- function(x, data,
+                             color.y = c("blue", "grey"),
+                             size.y = 2,
+                             color.yhat = c("red", "grey"),
+                             size.yhat = 2,
+                             ncol = 3,
+                             xlab = "Age (years)",
+                             ylab = "Length (SDS)",
+                             zband = TRUE,
+                             zband_range = NULL,
+                             xlim = NULL,
+                             ylim = NULL,
+                             theme = ggplot2::theme_light()) {
   g <- ggplot(data, aes_string(x = "x", y = "y")) +
     xlab(xlab) +
     ylab(ylab)
@@ -249,10 +160,10 @@ plot_trajectory_ggplot <- function(x, data,
   }
   if (zband) {
     g <- hbgd::geom_zband(g,
-      x = zband_range,
-      z = -c(2.5, 2, 1, 0),
-      color = zband_color,
-      alpha = 0.15
+                          x = zband_range,
+                          z = -c(2.5, 2, 1, 0),
+                          color = zband_color,
+                          alpha = 0.15
     )
   }
 
@@ -269,11 +180,11 @@ plot_trajectory_ggplot <- function(x, data,
   if (any(k)) {
     g <- g +
       geom_line(aes_string(y = "yhat"),
-        data = data[k, ], color = color.yhat[2]
+                data = data[k, ], color = color.yhat[2]
       ) +
       geom_point(aes_string(y = "yhat"),
-        data = data[k, ], color = color.yhat[1],
-        size = size.yhat
+                 data = data[k, ], color = color.yhat[1],
+                 size = size.yhat
       )
   }
 
@@ -284,22 +195,4 @@ plot_trajectory_ggplot <- function(x, data,
   g <- g + theme
 
   return(g)
-}
-
-
-#' Get the label of a HBGD standard variable
-#'
-#' This functions consults the \code{hbgd::hbgd_labels} table to find the variable
-#' label.
-#'
-#' @note This function should be placed in the \code{hbgd} package.
-#' @param x An character object with the name of one variable
-#' @return A character vector, either the label found in the table, or the name of the variable.
-#' @export
-get_label <- function(x) {
-  label <- hbgd::hbgd_labels[[x]]
-  if (is.null(label)) {
-    return(x)
-  }
-  return(label)
 }
