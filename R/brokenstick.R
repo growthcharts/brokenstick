d9dc719f6573a2f389f53bb8e95bbded0b143c89
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-16 21:13:20 +0200

    Shorten brokenstick()

--- R/brokenstick.R
+++ R/brokenstick.R
@@ -98,49 +98,8 @@ brokenstick <- function(y, x, subjid,
                         ...) {
   call <- match.call()
 
-  k_orig <- k
-  knots_orig <- knots
-  boundary_orig <- boundary
-
-  # if not specified, define boundary as data range
-  range <- range(x, na.rm = TRUE)
-  if (is.null(boundary_orig)) boundary <- range
-  if (length(boundary_orig) != 2) boundary <- range
-
-  # if knots is specified
-  #   extend lower boundary to min(knots)
-  #   extend upper boundary to max(knots)
-  if (!is.null(knots_orig)) {
-    boundary[1] <- min(min(knots_orig, na.rm = TRUE), boundary[1])
-    boundary[2] <- max(max(knots_orig, na.rm = TRUE), boundary[2])
-  }
-
-  # set k to zero if not specified
-  if (is.null(k_orig)) k <- 0
-
-  # if there is vector input via knots and if no k specified
-  # trim knots to exclude boundary points, and calculate k
-  if (is.null(k_orig) & length(knots_orig) >= 1) {
-    knots <- as.numeric(knots_orig)
-    knots <- knots[knots > boundary[1] & knots < boundary[2]]
-    k <- length(knots)
-  }
-
-  # for scalar k, calculate equidense quantiles from the data
-  if (!is.null(k_orig)) {
-    if (k_orig >= 0 & k_orig <= 25) {
-      k <- k_orig
-      knots <- quantile(x,
-        probs = seq(0, 1, length.out = k + 2),
-        na.rm = TRUE
-      )[-c(1, k + 2)]
-    }
-    else {
-      stop("Number of knots outside range 0-25")
-    }
-  }
-
-  X <- make_basis(x = x, knots = knots, boundary = boundary)
+  l <- calculate_knots(x, k, knots, boundary)
+  X <- make_basis(x = x, knots = l$knots, boundary = l$boundary)
 
   pred <- paste("0 +", paste(colnames(X), collapse = " + "))
   data <- data.frame(subjid = subjid, x = x, y = y, X)
@@ -156,8 +115,8 @@ brokenstick <- function(y, x, subjid,
   )
 
   class(fit) <- "brokenstick"
-  fit@knots <- as.numeric(knots)
-  fit@boundary <- as.numeric(boundary)
+  fit@knots <- as.numeric(l$knots)
+  fit@boundary <- as.numeric(l$boundary)
   fit@degree <- 1
   fit@bs.call <- call
   fit@xy <- data[, 1:3]
