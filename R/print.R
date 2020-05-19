7f638fa306866ab5c7f5d2c6a76fbd60fbe27f2d
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-19 23:38:34 +0200

    Add print function for brokenstick_export class

--- R/print.R
+++ R/print.R
@@ -12,3 +12,19 @@ print.brokenstick <- function(x, ..., digits = 2L) {
   cat("Residual variance: ", x$sigma2, "\n")
   invisible(x)
 }
+
+#' @export
+print.brokenstick_export <- function(x, ..., digits = 2L) {
+  cat(paste0("Class: brokenstick_export\n"))
+  cat("Names:", unname(unlist(x$names)), "\n")
+  cat("Knots:", sort(unique(c(x$knots, x$boundary))), "\n")
+  cat("Means:", round(x$beta, digits), "\n")
+  cat("Variance-covariance matrix:\n")
+  upper <- round(x$omega, digits)
+  upper[upper.tri(upper)] <- ""
+  upper <- as.data.frame(upper)
+  print(upper)
+  if (!is.na(x$sigma2j)) cat("Cluster residuals: ", x$sigma2j, "\n")
+  cat("Residual variance: ", x$sigma2, "\n")
+  invisible(x)
+}
