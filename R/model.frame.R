d578593e60f03ee539d04749717fca728297c678
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-19 18:17:29 +0200

    model.frame() extracts data, model.matrix() extracts design matrix

---
+++ R/model.frame.R
@@ -0,0 +1,14 @@
+#' @export
+model.frame.brokenstick <- function(formula, ...) {
+  formula$data
+}
+
+#' @export
+model.matrix.brokenstick <- function(object, ...) {
+  x_name <- object$names$x[1L]
+  make_basis(x = object$data[[x_name]],
+             knots = object$knots,
+             boundary = object$boundary,
+             knotnames = TRUE,
+             x_name = x_name)
+}
