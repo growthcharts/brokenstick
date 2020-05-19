59b07b37d5e6ad56c13f72689f13472eff6e16ee
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-19 18:15:27 +0200

    Use id in example data

--- data-raw/R/smocc_200.R
+++ data-raw/R/smocc_200.R
@@ -5,11 +5,10 @@ library(brokenstick)
 data <- load_data(dnr = "smocc", ids = 10001:11120)
 smocc_200 <- data$time[, c("id", "age", "sex", "ga", "bw" ,
                            "hgt", "hgt.z")]
-colnames(smocc_200)[1] <- "subjid"
 
 # fit the brokenstick model (takes about 20 seconds)
 fit_200 <- fit_brokenstick(data = smocc_200,
-                           formula = hgt.z ~ age | subjid,
+                           formula = hgt.z ~ age | id,
                            knots = round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24) / 12, 4),
                            boundary = c(0, 3))
 
