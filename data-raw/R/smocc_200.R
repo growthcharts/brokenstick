996c42129f90f7a87bad4370473b67e310cead8d
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-05-19 14:10:54 +0200

    Construct fit_200 object

--- data-raw/R/smocc_200.R
+++ data-raw/R/smocc_200.R
@@ -4,20 +4,14 @@ library(brokenstick)
 # get data
 data <- load_data(dnr = "smocc", ids = 10001:11120)
 smocc_200 <- data$time[, c("id", "age", "sex", "ga", "bw" ,
-                          "hgt", "hgt.z")]
+                           "hgt", "hgt.z")]
 colnames(smocc_200)[1] <- "subjid"
 
 # fit the brokenstick model (takes about 20 seconds)
-fit_200 <- with(
-  smocc_200,
-  brokenstick(
-    y = hgt.z,
-    x = age,
-    subjid = subjid,
-    knots = round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24) / 12, 4),
-    boundary = c(0, 3)
-  )
-)
+fit_200 <- fit_brokenstick(data = smocc_200,
+                           formula = hgt.z ~ age | subjid,
+                           knots = round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24) / 12, 4),
+                           boundary = c(0, 3))
 
 usethis::use_data(smocc_200, overwrite = TRUE)
 usethis::use_data(fit_200, overwrite = TRUE)
