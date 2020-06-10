7f11ffdff0ab729b3cef2182dd988638b8f72d75
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-06-10 22:34:28 +0200

    Revise plotting function so that they work with new classes

--- R/plot.R
+++ R/plot.R
@@ -1,25 +1,27 @@
-#' Plot observed and fitted trajectories
+#' Plot observed and fitted trajectories by group
 #'
-#' The \code{plot} method for a \code{brokenstick} object plots the observed and
-#' fitted trajectories of one or more individuals.
+#' The `plot` method for a `brokenstick` object plots the observed and
+#' fitted trajectories of one or more groups.
 #'
-#' @param x An object of class \code{brokenstick} or \code{brokenstick_export}
-#' @param py A vector with measurements using the same response scale as the
-#'   fitted model. Used as \code{y} in \code{\link{predict.brokenstick}}.
-#' @param px A vector with decimal ages of length \code{length(py)}.
-#'   fitted model. Used as \code{x} in \code{\link{predict.brokenstick}}.
-#' @param ids A vector with one or more subject identification codes.
-#' @param x_trim A range on the x-axis that can be used to subset values that are
-#' displayed
-#' @param max_ids A scalar indicating the number of individual plots. The default is 3, which plots the trajectories of the first three persons. The \code{max_ids} is a safety measure to prevent unintended plots of the entire data set.
-#' @param measurements A logical indicating whether the measurements should be plotted. The default is \code{TRUE}.
-#' @param estimates A logical indicating whether the broken stick estimates should be plotted.
-#' By default, it is \code{TRUE} if there are internal knots, and \code{FALSE} if there are
-#' only boundary knots.
-#' @param ... Extra arguments.
-#' @return An object of class \code{ggplot}.
+#' @param x An object of class `brokenstick`.
+#' @param .x The `x` argument of the [predict_new.brokenstick()] function.
+#' @param n_plot A integer indicating the number of individual plots.
+#' The default is 3, which plots the trajectories of the first three
+#' groups. The `n_plot` is a safety measure to prevent unintended
+#' plots of the entire data set.
+#' @param x_trim A range on the x-axis that can be used to subset values
+#' that are plotted.
+#' @param show A logical vector of length 2. The first element specifies
+#' whether the observed data are plotted, the second element specifies
+#' whether the added data (e.g. knots) are plotted. The default is
+#' `c(TRUE, TRUE)`.
+#' @param ... Extra arguments passed down to [predict_new.brokenstick()]
+#' and [plot_trajectory()].
+#' @inheritParams predict_new.brokenstick
+#' @return An object of class [ggplot2::ggplot].
 #' @author Stef van Buuren 2020
 #' @method plot brokenstick
+#' @seealso [predict_new.brokenstick], [plot_trajectory].
 #' @examples
 #' library("brokenstick")
 #' dat <- smocc_200
@@ -27,7 +29,7 @@
 #' fit <- brokenstick(hgt ~ age | id, dat)
 #'
 #' # plot measurements for first three cases
-#' #plot(fit, zband = FALSE, est = FALSE)
+#' # plot(fit_200, smocc_200, zband = FALSE, show = c(TRUE, FALSE))
 #'
 #' # fit model with knots at 1 and 2 years
 #' #fit <- fit_brokenstick(dat, hgt.z ~ age | id, knots = 1:2)
@@ -39,75 +41,40 @@
 #' #plot(fit, xlim = c(0, 2), ylim = c(-3, 3))
 #' #plot(fit, ids = 10005)
 #' @export
-plot.brokenstick <- function(x, py, px, ids = NULL,
-                             max_ids = 3,
+plot.brokenstick <- function(x,
+                             new_data,
+                             ...,
+                             .x = NULL,
+                             group = NULL,
+                             n_plot = 3,
                              x_trim = c(-Inf, Inf),
-                             measurements = TRUE,
-                             estimates = NULL, ...) {
+                             show = c(TRUE, TRUE)) {
   if (!inherits(x, "brokenstick")) stop("Argument `x` not of class brokenstick.")
 
-  # define default behavior of estimates flag
-  internal_knots <- get_knots(x, "knots")
-  if (is.null(estimates)) {
-    estimates <-
-      !is.null(internal_knots) & length(internal_knots) > 0
-  }
-
   # calculate brokenstick predictions, long format
-  if (estimates & missing(px)) px <- get_knots(x)
-  if (missing(py)) {
-    pr <- predict(object = x, x = px, ...)
-  } else {
-    pr <- predict(object = x, y = py, x = px, ...)
-  }
+  if (show[2L] && missing(.x)) .x <- "knots"
+  data <- predict_new(object = x, new_data = new_data, x = .x,
+                      group = group, strip_data = FALSE, ...)
 
-  # plot first max_ids trajectories if ids == NULL
-  if (is.null(ids)) {
-    idx <- pr[[x$names$z]] %in% unique(pr[[x$names$z]])[1:max_ids]
-  } else {
-    idx <- pr[[x$names$z]] %in% ids
-  }
-  idx <- idx & pr[[x$names$x]] >= x_trim[1] & pr[[x$names$x]] <= x_trim[2]
-  data <- pr[idx, , drop = FALSE]
+  # apply trim
+  idx <- data[[x$names$x]] >= x_trim[1L] & data[[x$names$x]] <= x_trim[2L]
+  data <- data[idx, , drop = FALSE]
 
   plot_trajectory(x = x, data = data, ...)
 }
 
-#' @inheritParams plot.brokenstick
-#' @method plot brokenstick_export
-#' @export
-plot.brokenstick_export <- function(x, py, px, ids = NULL,
-                                    x_trim = c(-Inf, Inf), max_ids = 3, ...) {
-  if (!inherits(x, "brokenstick_export")) stop("Argument `x` not of class brokenstick.")
-
-  # calculate brokenstick predictions, long format
-  mpy <- missing(py)
-  mpx <- missing(px)
-  if (mpy & mpy) {
-    pr <- predict(object = x, ...)
-  } else if (mpy) {
-    pr <- predict(object = x, x = px, ...)
-  } else if (mpx) {
-    pr <- predict(object = x, y = py, ...)
-  } else {
-    pr <- predict(object = x, y = py, x = px, ...)
-  }
-
-  idx <- pr[[x$names$x]] >= x_trim[1] & pr[[x$names$x]] <= x_trim[2]
-  data <- pr[idx, , drop = FALSE]
-  plot_trajectory(x = x, data = data, ...)
-}
 
 #' Plot observed and fitted trajectories from fitted brokenstick model
 #'
 #' This function plot the observed and fitted trajectories from brokenstick model.
-#' Actual plotting may be done by \code{gglot} or \code{rbokeh}.
-#' @param x An object of class \code{brokenstick} or \code{brokenstick_export}
-#' @param data A list of data frames produced by \code{predict.brokenstick} and \code{predict.brokenstick_export}
-#' @param color.y A character vector with two elements specifying the symbol and line color of the measured data points
-#' @param size.y Dot size of measured data points
-#' @param color.yhat A character vector with two elements specifying the symbol and line color of the predicted data points
-#' @param size.yhat Dot size of predicted data points
+#' Actual plotting may be done by `gglot`.
+#' @param x An object of class `brokenstick`.
+#' @param data A `data.frame` with columns names `x$names`, `".source"`
+#' and `".pred"`.
+#' @param color_y A character vector with two elements specifying the symbol and line color of the measured data points
+#' @param size_y Dot size of measured data points
+#' @param color_yhat A character vector with two elements specifying the symbol and line color of the predicted data points
+#' @param size_yhat Dot size of predicted data points
 #' @param ncol Number of columns in plot
 #' @param xlab The label of the x-axis
 #' @param ylab The label of the y-axis
@@ -115,25 +82,22 @@ plot.brokenstick_export <- function(x, py, px, ids = NULL,
 #' @param ylim Vector of length 2 with range of y-axis
 #' @param theme Plotting theme
 #' @param zband A logical indicating whether the Z-score band should be
-#' added to the plot. The default is \code{TRUE}.
+#' added to the plot. The default is `TRUE`.
 #' @param zband_range a vector specifying the range (min, max) that the superposed growth standard should span on the x-axis. The
 #' default is the entire data range.
 #' @return An object of class \code{ggplot}
 #' @rdname plot_trajectory
+#' @seealso [plot.brokenstick]
 #' @examples
-#' smc <- brokenstick::smocc_200
-#' knots <- 0:2
-#' #fit <- fit_brokenstick(smc, hgt.z ~ age | id, knots = knots)
-#'
 #' # plot first three cases
-#' #plot(fit)
+#' # plot(fit_200, brokenstick::smocc_200)
 #' @export
 plot_trajectory  <- function(x, data,
-                             color.y = c("blue", "grey"),
-                             size.y = 2,
-                             color.yhat = c("red", "grey"),
-                             size.yhat = 2,
-                             ncol = 3,
+                             color_y = c("blue", "grey"),
+                             size_y = 2,
+                             color_yhat = c("red", "grey"),
+                             size_yhat = 2,
+                             ncol = 3L,
                              xlab = "Age (years)",
                              ylab = "Length (SDS)",
                              zband = TRUE,
@@ -167,32 +131,30 @@ plot_trajectory  <- function(x, data,
     )
   }
 
-
   # add observed data points and lines
-  k <- data$knot
+  k <- data$.source == "added"
   if (any(!k)) {
     g <- g +
-      geom_line(data = data[!k, ], color = color.y[2]) +
-      geom_point(data = data[!k, ], color = color.y[1], size = size.y)
+      geom_line(data = data[!k, ], color = color_y[2L]) +
+      geom_point(data = data[!k, ], color = color_y[1L], size = size_y)
   }
 
   # add broken stick points and lines
   if (any(k)) {
     g <- g +
-      geom_line(aes_string(y = "yhat"),
-                data = data[k, ], color = color.yhat[2]
+      geom_line(aes_string(y = ".pred"),
+                data = data[k, ], color = color_yhat[2L]
       ) +
-      geom_point(aes_string(y = "yhat"),
-                 data = data[k, ], color = color.yhat[1],
-                 size = size.yhat
+      geom_point(aes_string(y = ".pred"),
+                 data = data[k, ], color = color_yhat[1L],
+                 size = size_yhat
       )
   }
 
   # split out according to subjid
-  g <- g + facet_wrap(as.formula(paste("~", x$names$z)), ncol = ncol)
-
-  # set theme
-  g <- g + theme
+  g <- g +
+    facet_wrap(as.formula(paste("~", x$names$g)), ncol = ncol) +
+    theme
 
   return(g)
 }
