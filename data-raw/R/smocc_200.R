235476b6ebdfb7af0c100d58319672f32c83b373
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-06-12 14:49:42 +0200

    Update fit_200

--- data-raw/R/smocc_200.R
+++ data-raw/R/smocc_200.R
@@ -7,10 +7,9 @@ smocc_200 <- data$time[, c("id", "age", "sex", "ga", "bw" ,
                            "hgt", "hgt.z")]
 
 # fit the brokenstick model (takes about 20 seconds)
-fit_200 <- fit_brokenstick(data = smocc_200,
-                           formula = hgt.z ~ age | id,
-                           knots = round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24) / 12, 4),
-                           boundary = c(0, 3))
+knots <- round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24)/12, 4)
+fit_200 <- brokenstick(hgt.z ~ age | id, data = smocc_200,
+                       knots = knots, boundary = c(0, 3))
 
 usethis::use_data(smocc_200, overwrite = TRUE)
 usethis::use_data(fit_200, overwrite = TRUE)
