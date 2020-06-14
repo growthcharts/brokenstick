3ca55cd87fa8f3723a7f7974fc022f44fa0731a0
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-06-14 22:08:45 +0200

    Remove get_X() and get_data()

--- R/get.R
+++ R/get.R
@@ -39,49 +39,6 @@ get_knots <- function(object,
   return(result)
 }
 
-#' Obtain the data from a broken stick model
-#'
-#' @param object An object of class \code{brokenstick}
-#' @param ids A vector specifying the id's of the persons. If omitted,
-#' all id's are included.
-#' @return A data frame with x, y and z. The result is \code{NULL}
-#' if \code{object} is not of class \code{brokenstick}.
-#' @examples
-#' get_data(fit_200, ids = c(10001, 10002))
-#' @export
-get_data <- function(object, ids = NULL) {
-  if (!inherits(object, "brokenstick")) {
-    return(NULL)
-  }
-  mf <- model.frame(object)
-  if (!is.null(ids)) {
-    mf <- mf[mf[[object$names$z]] %in% ids, ]
-  }
-  mf
-}
-
-#' Obtain the X model matrix from a broken stick model
-#'
-#' @param object An object of class \code{brokenstick}
-#' @param ids A vector specifying the id's of the persons. If omitted,
-#' all id's are included.
-#' @return A matrix with number of columns equal to the number of
-#' knots. The result is \code{NULL} if \code{object} is not of class
-#' \code{brokenstick}.
-#' @examples
-#' # get_X(fit_200, ids = c(10001, 10002))
-#' @export
-get_X <- function(object, ids = NULL) {
-  if (!inherits(object, "brokenstick")) {
-    return(NULL)
-  }
-  if (is.null(ids)) {
-    return(model.matrix(object$model))
-  }
-  grp <- model.frame(object$model)[[object$names$z]]
-  idx <- grp %in% ids
-  return(model.matrix(object$model)[idx, , drop = FALSE])
-}
 
 #' Obtain proportion of explained variance from a broken stick model
 #'
