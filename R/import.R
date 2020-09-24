5263d221eb9260f8145f911d1094928f91a243cc
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-09-24 09:41:23 +0200

    Make seperate import.R

---
+++ R/import.R
@@ -0,0 +1,16 @@
+#' @importFrom dplyr           bind_rows bind_cols filter group_by
+#'                             mutate pull relocate select ungroup `%>%`
+#' @importFrom lme4            fixef lmer lmerControl ranef VarCorr
+#' @importFrom methods         slot
+#' @importFrom matrixsampling  rwishart
+#' @importFrom rlang           .data arg_match
+#' @importFrom splines         bs
+#' @importFrom stats           approx as.formula cor cov2cor
+#'                             fitted model.frame lm
+#'                             model.matrix na.exclude na.omit
+#'                             na.pass optim
+#'                             predict quantile rgamma rnorm
+#'                             rWishart setNames smooth var
+#' @importFrom tidyr           drop_na pivot_wider
+#' @importFrom utils           askYesNo install.packages
+NULL
