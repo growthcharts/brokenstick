a729f6569b9a6e3c89e1b6bab112a391699c4050
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-04 20:32:18 +0200

    Adapt residuals() to light object

--- R/residuals.R
+++ R/residuals.R
@@ -3,10 +3,16 @@
 #' @aliases residuals.brokenstick
 #' @inheritParams predict.brokenstick
 #' @param \dots Additional arguments. Ignored.
-#' @return See [predict.brokenstick()].
+#' @return A data.frame with a column named `.resid`
 #' @family brokenstick
 #' @export
 residuals.brokenstick <- function(object, newdata = NULL, ...) {
+  if (is.null(newdata) && object$light) {
+    stop("A light brokenstick object expects a `newdata` argument.", call. = FALSE)
+  }
+  if (is.null(newdata) && !object$light) {
+    newdata <- object$data
+  }
   pred <- predict(object, newdata)
   y <- newdata[[object$names$y]]
   r <- y - pred
