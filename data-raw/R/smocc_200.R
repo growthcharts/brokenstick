9fc42835adac624e53b6fbc5cc82a08742b8b3e7
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-10 17:27:11 +0200

    Correct comment

--- data-raw/R/smocc_200.R
+++ data-raw/R/smocc_200.R
@@ -6,7 +6,7 @@ data <- load_data(dnr = "smocc", ids = 10001:11120)
 smocc_200 <- data$time[, c("id", "age", "sex", "ga", "bw" ,
                            "hgt", "hgt_z")]
 
-# fit the brokenstick model (takes about 20 seconds)
+# fit the brokenstick model
 knots <- round(c(0, 1, 2, 3, 6, 9, 12, 15, 18, 24)/12, 4)
 fit_200 <- brokenstick(hgt_z ~ age | id, data = smocc_200,
                        knots = knots, boundary = c(0, 3), seed = 1)
