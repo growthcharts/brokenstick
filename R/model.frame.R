b25961765700eafe53133a91d64dc3c4026a0099
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-11-03 23:47:09 +0100

    Adapt to light object

--- R/model.frame.R
+++ R/model.frame.R
@@ -1,10 +1,12 @@
 #' @export
 model.frame.brokenstick <- function(formula, ...) {
+  if (formula$light) return(NULL)
   return(formula$data)
 }
 
 #' @export
 model.matrix.brokenstick <- function(object, ...) {
+  if (object$light) return(NULL)
   x_name <- object$names$x[1L]
   obj <- make_basis(x = object$data[[x_name]],
                     internal = get_knots(object, "internal"),
